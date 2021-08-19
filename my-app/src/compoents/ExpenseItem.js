cbaa219ba6ee667f17eaa44238ebd297561aafc3
mohsenbahrami <mohsenbahrami_ah@yahoo.com>
2021-08-19 18:19:05 -0400

    add

---
+++ my-app/src/compoents/ExpenseItem.js
@@ -0,0 +1,15 @@
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Data</h2>
+      </div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>
+        <h2>Amount</h2>
+      </div>
+    </div>
+  );
+}
